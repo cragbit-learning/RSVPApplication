c9519091ddbc38b0be00aa35e531b2b5eed4efe5
sanjiv <sanjiv.kumar@happiestminds.com>
2019-04-22 11:41:17 +0530

    putting comments

--- src/main/java/com/sanjiv/rsvp/RsvpApplication.java
+++ src/main/java/com/sanjiv/rsvp/RsvpApplication.java
@@ -10,11 +10,13 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 @SpringBootApplication
 public class RsvpApplication {
 
-	private static final String MEETUP_RSVPS_ENDPOINT = "ws://stream.meetup.com/2/rsvps";
+    private static final String MEETUP_RSVPS_ENDPOINT = "ws://stream.meetup.com/2/rsvps";
 
     public static void main(String[] args) {
         SpringApplication.run(RsvpApplication.class, args);
     }
+
+    /* Bean initialize */
 
     @Bean
     public ApplicationRunner initializeConnection(
